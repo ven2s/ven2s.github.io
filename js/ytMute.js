d1dfe96716d3154a8a0e5658d9db17fe5b33e9de
강준 <wajl1004@gmail.com>
2017-07-10 17:04:28 +0900

    fixed youtube javascript

--- js/ytMute.js
+++ js/ytMute.js
@@ -3,7 +3,7 @@
 var tag = document.createElement('script');
  
     tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
+    var firstScriptTag = document.getElementsByTagName('script')[10];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
  
     var player;
